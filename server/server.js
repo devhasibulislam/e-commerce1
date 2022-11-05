e7801807d86bf7f906573a36d6f2080227833033
hasibulislam999 <hasib143sl@gmail.com>
2022-11-05 21:12:39 +0600

    application server initialized

---
+++ server/server.js
@@ -0,0 +1,33 @@
+/**
+ * Title: Server level initialization
+ * Description: Execute database and port of server intentionally
+ * Author: Hasibul Islam
+ * Date: 05/11/2022
+ */
+
+/* external imports */
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+/* internal imports */
+const app = require("./app");
+const consoleMessage = require("./utils/consoleMessage.util");
+
+/* database connection */
+mongoose
+  .connect(process.env.URI_STRING, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    consoleMessage.successMessage(
+      `Establish connection on ${process.env.URI_STRING}`
+    )
+  )
+  .catch((error) => consoleMessage.errorMessage(error.message));
+
+/* establish server port */
+app.listen(process.env.PORT, () => {
+  consoleMessage.successMessage(`App listening on http://localhost:8080/`);
+});
