03da96492f86d9e6eebecb537aa7e97904fb2560
hasibulislam999 <hasib143sl@gmail.com>
2022-11-07 16:54:16 +0600

    navbar component modified

--- client/src/shared/navbar/Navbar.js
+++ client/src/shared/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar1 from "./Navbar1";
 import Navbar2 from "./Navbar2";
+import Navbar3 from "./Navbar3";
 
 const Navbar = () => {
   return (
     <header>
       <Navbar1 />
       <Navbar2 />
+      <Navbar3 />
     </header>
   );
 };
