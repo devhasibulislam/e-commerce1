9597eb41adff56e9c624b6e172d1a78aaba88945
hasibulislam999 <hasib143sl@gmail.com>
2022-11-08 23:21:48 +0600

    route initialize

---
+++ client/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "../shared/footer/Footer";
+import Navbar from "../shared/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      {/* page header */}
+      <Navbar />
+
+      {/* page contents */}
+      <main>
+        
+      </main>
+
+      {/* page footer */}
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
