26470917d423bca9872535659692abd5d021367a
hasibulislam999 <hasib143sl@gmail.com>
2022-11-08 21:41:05 +0600

    not found page integrated

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/NotFound.css";
+
+const NotFound = () => {
+  return (
+    <section className="page_404 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:w-1/2 md:w-1/2 w-full shadow rounded">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <div className="four_zero_four_bg">
+                <h1 className="text-center ">404</h1>
+              </div>
+
+              <div className="contant_box_404">
+                <h3 className="h2">Look like you're lost</h3>
+
+                <p>the page you are looking for not avaible!</p>
+
+                <Link to={"/"} className="link_404 rounded">
+                  Go to home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
