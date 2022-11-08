57f7a2ba8cda3683dde4be62e2167fc7cb7b8bd0
hasibulislam999 <hasib143sl@gmail.com>
2022-11-08 23:21:39 +0600

    updated throughout home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <section className="App">
       <Routes>
+        {/* home or root page */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+
+        {/* not found page */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
