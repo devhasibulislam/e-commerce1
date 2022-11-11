e6c8c79a61b0df2442eec648794ba70d674270d2
hasibulislam999 <hasib143sl@gmail.com>
2022-11-11 12:36:30 +0600

    modified throughout login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <section className="App">
       <Routes>
-        {/* home or root page */}
+        {/* home or root pages */}
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+
+        {/* authentications pages */}
+        <Route path="/login" element={<Login />} />
 
         {/* not found page */}
         <Route path="*" element={<NotFound />} />
