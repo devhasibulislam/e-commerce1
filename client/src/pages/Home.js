cbf38a41bf431b8eaf752d35c5900600690e441a
sh-mithu <sabbirhasanmithu420@gmail.com>
2022-11-11 19:00:45 +0600

    home route modified

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../components/home/Banner";
 import BigDiscounts from "../components/home/BigDiscounts";
 import CycleStyle from "../components/home/CycleStyle";
@@ -8,10 +8,15 @@ import NewArrivals from "../components/home/NewArrivals";
 import TabCategory from "../components/home/TabCategory";
 import TopCategories from "../components/home/TopCategories";
 import TopRatingsFeaturedBrands from "../components/home/TopRatingsFeaturedBrands";
+import Modal from "../components/Modal";
+import ModalContent from "../components/ModalContent";
 import Footer from "../shared/footer/Footer";
 import Navbar from "../shared/navbar/Navbar";
 
 const Home = () => {
+  const [product, setProduct] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
       {/* page header */}
@@ -20,15 +25,23 @@ const Home = () => {
       {/* page contents */}
       <main>
         <Banner />
-        <FlashDeals />
+        <FlashDeals setShowModal={setShowModal} setProduct={setProduct} />
         <TopCategories />
         <TopRatingsFeaturedBrands />
         <NewArrivals />
         <BigDiscounts />
-        <TabCategory />
-        <MadeForYou />
+        <TabCategory setShowModal={setShowModal} setProduct={setProduct} />
+        <MadeForYou setShowModal={setShowModal} setProduct={setProduct} />
         <CycleStyle />
       </main>
+
+      {showModal && (
+        <Modal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          content={<ModalContent product={product} />}
+        />
+      )}
 
       {/* page footer */}
       <Footer />
