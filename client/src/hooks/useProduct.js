a1846bcb29a5a1a3138df66ceeefd0f3fee9434c
hasibulislam999 <hasib143sl@gmail.com>
2022-11-11 21:25:08 +0600

    use product hook modified

--- client/src/hooks/useProduct.js
+++ client/src/hooks/useProduct.js
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 
 function useProduct() {
   const [products, setProducts] = useState([]);
+  const [temp, setTemp] = useState({});
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        setProducts(data);
+        setTemp(data); // refetch at certain interval
+      })
       .catch((err) => console.error(err));
-  }, []);
+  }, [temp]);
 
   return products;
 }
