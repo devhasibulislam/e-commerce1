e99016e3596bef657e5748b88898be2217f385bb
hasibulislam999 <hasib143sl@gmail.com>
2022-11-11 12:42:36 +0600

    updated throughout reset route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Reset from "./pages/Reset";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
 
         {/* authentications pages */}
         <Route path="/login" element={<Login />} />
+        <Route path="/reset" element={<Reset />} />
 
         {/* not found page */}
         <Route path="*" element={<NotFound />} />
