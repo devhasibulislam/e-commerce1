08e76746060cef8ef7849d8280da435b408aba62
sayemsomrat01 <sayemsomrat01@gmail.com>
2022-11-11 09:46:50 +0600

    modified throughout made for you

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/home/Banner";
 import BigDiscounts from "../components/home/BigDiscounts";
 import FlashDeals from "../components/home/FlashDeals";
+import MadeForYou from "../components/home/MadeForYou";
 import NewArrivals from "../components/home/NewArrivals";
 import TabCategory from "../components/home/TabCategory";
 import TopCategories from "../components/home/TopCategories";
@@ -24,6 +25,7 @@ const Home = () => {
         <NewArrivals />
         <BigDiscounts />
         <TabCategory />
+        <MadeForYou />
       </main>
 
       {/* page footer */}
