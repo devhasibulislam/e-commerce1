1a6378057a4969a3da1a95443b18c1dd6299a4ce
hasibulislam999 <hasib143sl@gmail.com>
2022-11-11 16:05:53 +0600

    updated throughout register route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Register from "./pages/Register";
 import Reset from "./pages/Reset";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         {/* authentications pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/reset" element={<Reset />} />
+        <Route path="/register" element={<Register />} />
 
         {/* not found page */}
         <Route path="*" element={<NotFound />} />
