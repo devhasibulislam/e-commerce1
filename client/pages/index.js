5cd4e89542beb15e57ad982ab1fc6608a264b5a2
hasibulislam999 <hasib143sl@gmail.com>
2022-11-15 22:17:34 +0600

    update through adding title on top tab

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Title from "../components/Title";
 import Header from "../shared/Header";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
     <section className={styles.default__font}>
+      <Title title={"Home"} />
       <Header />
     </section>
   );
