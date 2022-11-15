9fe6a56d0e62b5d3b58fbe217ac7d227bd3fb71f
hasibulislam999 <hasib143sl@gmail.com>
2022-11-15 20:30:51 +0600

    shared component profile submenu init

---
+++ client/shared/ProfileSubmenu.js
@@ -0,0 +1,94 @@
+import Link from "next/link";
+import React from "react";
+
+const ProfileSubmenu = () => {
+  // user profile options
+  const profileOptions = [
+    {
+      anchor: "/dashboard",
+      title: "Dashboard",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 text-[#009f46]"
+        >
+          <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z" />
+        </svg>
+      ),
+    },
+    {
+      anchor: "/my-profile",
+      title: "My Profile",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 text-[#e56c36]"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      anchor: "/sign-in",
+      title: "Sign in",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 text-[#009f46]"
+        >
+          <path
+            fillRule="evenodd"
+            d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div
+        className={`flex flex-col gap-y-4 bg-white border border-[#009f46] shadow-lg p-4 rounded-lg absolute top-full right-[50%] z-50`}
+      >
+        {profileOptions.map((profileOption, index) => (
+          <Link
+            key={index}
+            href={profileOption.anchor}
+            className="flex gap-x-2 shadow px-4 py-2 hover:shadow-lg"
+          >
+            {profileOption.icon}
+            {profileOption.title}
+          </Link>
+        ))}
+        <p className="flex gap-x-2 shadow px-4 py-2 hover:shadow-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6 text-red-500"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Logout
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ProfileSubmenu;
