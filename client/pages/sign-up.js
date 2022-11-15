62d1f6d0913835b8a8999d2aba278677838c9169
hasibulislam999 <hasib143sl@gmail.com>
2022-11-15 22:17:59 +0600

    sign up route init

---
+++ client/pages/sign-up.js
@@ -0,0 +1,147 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Title from "../components/Title";
+import AccountBanner from "../components/AccountBanner";
+
+const Signup = () => {
+  return (
+    <section className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <Title title={"Sign up"} />
+      <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+        <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
+          <div className="w-fit mx-auto">
+            <Link href="/">
+              <Image
+                src={"/logo.png"}
+                height={39}
+                width={128}
+                alt="E-Commerce Logo"
+                title="E-Commerce Logo"
+                className="w-32 mx-auto"
+              />
+            </Link>
+          </div>
+          <div className="mt-12 flex flex-col items-center">
+            <h1 className="text-2xl xl:text-3xl font-extrabold">
+              Sign up as a new user
+            </h1>
+            <div className="w-full flex-1">
+              <div className="mx-auto max-w-xs mt-8">
+                <form className="flex flex-col gap-y-4">
+                  {/* name field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="text"
+                    name="name"
+                    placeholder="Full Name"
+                  />
+
+                  {/* email field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+
+                  {/* password field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                  />
+
+                  {/* confirm password field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                  />
+
+                  {/* phone number field */}
+                  <input
+                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                    type="number"
+                    name="phone"
+                    placeholder="Phone"
+                  />
+
+                  {/* avatar upload field */}
+                  <div className="flex items-center justify-center w-full">
+                    <label
+                      for="dropzone-file"
+                      className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                    >
+                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg
+                          aria-hidden="true"
+                          className="w-10 h-10 mb-3 text-gray-400"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                          ></path>
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                          <span className="font-semibold">Click to upload</span>{" "}
+                          or drag and drop
+                        </p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">
+                          SVG, PNG, JPG or WEBP (MAX. 800x400px)
+                        </p>
+                      </div>
+                      <input
+                        id="dropzone-file"
+                        type="file"
+                        className="hidden"
+                      />
+                    </label>
+                  </div>
+
+                  {/* submit button */}
+                  <button className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                    <svg
+                      className="w-6 h-6 -ml-2"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                      <circle cx="8.5" cy="7" r="4" />
+                      <path d="M20 8v6M23 11h-6" />
+                    </svg>
+                    <span className="ml-3">Sign Up</span>
+                  </button>
+                </form>
+                <p className="mt-6 text-xs text-gray-600 text-center">
+                  Already have an account?
+                  <Link
+                    href="/sign-in"
+                    className="border-b border-gray-500 border-dotted"
+                  >
+                    {" "}
+                    Login here
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <AccountBanner />
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
