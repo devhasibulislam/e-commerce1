df91999f95efb05c87b8583fac45bd3a98cccdc9
hasibulislam999 <hasib143sl@gmail.com>
2022-11-16 17:07:35 +0600

    refactor context api on header component

--- client/shared/Header.js
+++ client/shared/Header.js
@@ -24,66 +24,66 @@ const Header = () => {
   );
 
   return (
-    <header className="shadow py-4">
-      <nav className="container mx-auto lg:px-0 px-4">
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex items-center gap-x-4">
-            {/* header logo */}
-            <Logo />
-
-            {/* vertical line */}
-            {verticalLine}
-
-            {/* header category */}
-            <Categories />
-          </div>
-
-          {/* menu bar */}
-          <div
-            className="lg:hidden shadow-md hover:shadow-lg p-2 rounded-full relative"
-            title="Click to display all options..."
-            onMouseEnter={() => setOpenSubMenu(true)}
-            onMouseLeave={() => setOpenSubMenu(false)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 text-[#e56c36]"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
-              />
-            </svg>
-
-            {openSubMenu && <Submenu />}
-          </div>
-
-          <div className="lg:flex lg:items-center lg:gap-x-4 hidden">
-            {/* header search bar */}
-            <Searchbar />
-
-            {/* header navigation */}
-            <div className="flex justify-between items-center gap-x-4">
-              {/* shopping cart */}
-              <Cart />
+    <ExistingUser.Provider value={myselfState}>
+      <header className="shadow py-4">
+        <nav className="container mx-auto lg:px-0 px-4">
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex items-center gap-x-4">
+              {/* header logo */}
+              <Logo />
 
               {/* vertical line */}
               {verticalLine}
 
-              {/* user profile */}
-              <ExistingUser.Provider value={myselfState}>
+              {/* header category */}
+              <Categories />
+            </div>
+
+            {/* menu bar */}
+            <div
+              className="lg:hidden shadow-md hover:shadow-lg p-2 rounded-full relative"
+              title="Click to display all options..."
+              onMouseEnter={() => setOpenSubMenu(true)}
+              onMouseLeave={() => setOpenSubMenu(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 text-[#e56c36]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+                />
+              </svg>
+
+              {openSubMenu && <Submenu />}
+            </div>
+
+            <div className="lg:flex lg:items-center lg:gap-x-4 hidden">
+              {/* header search bar */}
+              <Searchbar />
+
+              {/* header navigation */}
+              <div className="flex justify-between items-center gap-x-4">
+                {/* shopping cart */}
+                <Cart />
+
+                {/* vertical line */}
+                {verticalLine}
+
+                {/* user profile */}
                 <Profile />
-              </ExistingUser.Provider>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </ExistingUser.Provider>
   );
 };
 
