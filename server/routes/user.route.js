a95cecc017aae9af61e01886aa7a8f12b8ab082e
hasibulislam999 <hasib143sl@gmail.com>
2022-11-16 13:35:51 +0600

    user route init

---
+++ server/routes/user.route.js
@@ -0,0 +1,63 @@
+/**
+ * Title: User authenticate credentials
+ * Description: All user credential convey execute from here
+ * Author: Hasibul Islam
+ * Date: 26/11/2022
+ */
+
+/* external import */
+const express = require("express");
+
+/* internal import */
+const userController = require("../controllers/user.controller");
+const verifyTokenMiddleware = require("../middlewares/verifyToken.middleware");
+const authorizeRoleMiddleware = require("../middlewares/authorizeRole.middleware");
+const uploader = require("../middlewares/cloudinaryUpload.middleware");
+
+/* router level connection */
+const router = express.Router();
+
+/* router methods integration */
+// upload user avatar
+router.post(
+  "/avatar",
+  uploader.single("avatar"),
+  userController.cloudinaryUpload
+);
+
+// sign up an user with confirmation
+router
+  .route("/sign-up")
+  .get(userController.confirmSignedUpUser)
+  .post(userController.signUpAnUser);
+
+//   sign in an user
+router.post("/sign-in", userController.signInAnUser);
+
+// persist an user to logged in
+router.get("/myself", verifyTokenMiddleware, userController.persistMeLogin);
+
+// fetch all user
+router.get(
+  "/all-users",
+  verifyTokenMiddleware,
+  authorizeRoleMiddleware("admin"),
+  userController.displayAllUsers
+);
+
+// reset password
+router
+  .route("/reset-password")
+  .get(userController.confirmPasswordReset)
+  .patch(userController.forgotPassword);
+
+// remove an user account
+router.delete(
+  "/remove-user",
+  verifyTokenMiddleware,
+  authorizeRoleMiddleware("admin"),
+  userController.removeAnUser
+);
+
+/* export user router */
+module.exports = router;
